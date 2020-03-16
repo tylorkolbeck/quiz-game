acba7647213db03a4cf790876ea2291dafa3adb1
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-03-15 18:53:54 -0700

    inital commit

---
+++ assets/questions/questions.js
@@ -0,0 +1,12 @@
+let questionsArrayRaw = [
+    {
+        answer: 0,
+        question: 'What color is the sky?',
+        answers: ['Blue', 'Green', 'Purple', 'Trick Question']
+    },
+    {
+        answer: 3,
+        question: 'What is your favorite color?',
+        answers: ['Magenta', 'Green', 'Cyan', 'Gold']
+    }
+] 
---
+++ assets/scripts/main.js
@@ -0,0 +1,92 @@
+
+// WARNING GRAPHIC CONTENT - CLOSE YOUR EYES
+// This questions array is being grabbed from global scope which
+// is being loaded in by a script tag in index.html
+let questionsArray = [...questionsArrayRaw]
+// END GRAPHIC CONTENT - YOU CAN OPEN YOUR EYES NOW
+
+let questionHeaderEl = document.getElementById("game-card-question")
+let answersListEl = document.getElementById("game-card-answer-list")
+let timerEl = document.getElementById("timer")
+let solutionEl = document.getElementById("game-card-solution")
+
+answersListEl.addEventListener("click", handleAnswerSelection)
+
+let timeRemaing = 59
+let currentQuestionIndex = 0
+let answeredCorrectly = 0
+let answeredWrong = 0
+let answers = []
+let timerId = setInterval(countDownHandler, 1000)
+
+function countDownHandler() {
+    if (timeRemaing > 1) {
+        timeRemaing--
+        timerEl.textContent = `Timer: ${timeRemaing}s`
+    } else {
+        clearInterval(timerId)
+    }
+}
+
+updateUI()
+
+function handleAnswerSelection(event) {
+    if (event.target.nodeName === 'LI') {
+        let answerIndex = parseInt(event.target.getAttribute('data-index'))
+        checkRightOrWrong(answerIndex)
+    }
+}
+
+function checkRightOrWrong(answerIndex) {
+    if (questionsArray[currentQuestionIndex].answer === answerIndex) {
+        answeredCorrectly++
+        timeRemaing = timeRemaing + 5
+        solutionEl.textContent = 'Correct! +5 secs'
+    } else {
+        answeredWrong++
+        timeRemaing = timeRemaing - 10
+        solutionEl.textContent = 'Wrong -10 secs'
+    }
+    currentQuestionIndex += 1
+
+    setTimeout(showAnswer, 1000)
+
+    solutionEl.style.display = 'block'
+
+    function showAnswer() {
+        // Check to see if it was the last question
+        if (currentQuestionIndex < questionsArray.length) {
+            updateUI()
+        } else {
+            clearUI()
+            clearInterval(timerId)
+        }
+    }
+}
+
+function updateUI() {
+    clearUI()
+
+    questionHeaderEl.textContent = `${currentQuestionIndex} of ${questionsArray.length}: ${questionsArray[currentQuestionIndex].question}`
+    
+    answers = questionsArray[currentQuestionIndex].answers.map((answer, index) => {
+        let li = document.createElement("li")
+        li.setAttribute("data-index", index)
+        li.textContent = answer
+        return li
+    })
+
+    answers.forEach(answerEl => answersListEl.appendChild(answerEl))
+}
+
+function clearUI() {
+    solutionEl.style.display = 'none'
+    answers = []
+    answersListEl.innerHTML = ""
+    questionHeaderEl.innerHTML = ""
+}
+
+function endGameAndShowScore() {
+
+}
+
