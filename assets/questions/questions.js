43b066380047831ad051ba6d1c777ff60605175a
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-03-15 19:03:50 -0700

    Added some more questions

--- assets/questions/questions.js
+++ assets/questions/questions.js
@@ -1,12 +1,27 @@
 let questionsArrayRaw = [
     {
-        answer: 0,
-        question: 'What color is the sky?',
-        answers: ['Blue', 'Green', 'Purple', 'Trick Question']
+        answer: 1,
+        question: 'What does the method Array.prototype.forEach() return?',
+        answers: ['null', 'undefined', 'the original array', 'a new array']
+    },
+    {
+        answer: 1,
+        question: 'T/F: Array.prototype.push() returns the array that was modified?',
+        answers: ['True', 'False']
     },
     {
         answer: 3,
-        question: 'What is your favorite color?',
-        answers: ['Magenta', 'Green', 'Cyan', 'Gold']
+        question: 'What property do you use to find the length of an array?',
+        answers: ['.includes()', '.length()', '.hasOwnProperty()', '.length']
+    },
+    {
+        answer: 1,
+        question: 'T/F: Array.prototype.findIndex() returns false when it does not find a match',
+        answers: ['True', 'False']
+    },
+    {
+        answer: 1,
+        question: 'What does Array.prototype.splice() return?',
+        answers: ['undefined', 'New Array With Deleted Elements', 'Original Array Without Deleted Elements', 'An Empty Array']
     }
 ] 
