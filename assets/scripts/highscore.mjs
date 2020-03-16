b5c5aa302a525d82ab021fefdab9f99948ebb3f4
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-03-16 14:21:42 -0700

    backbutton was not being referenced properly.  Now added an ID to the element

--- assets/scripts/highscore.mjs
+++ assets/scripts/highscore.mjs
@@ -1,9 +1,9 @@
 let scoresListEl = document.getElementById('high-scores-list')
 let scores = JSON.parse(localStorage.getItem('quizGame'))
 
-document.getElementsByClassName("go-home-btn")
+let backButton = document.getElementById("back-btn")
 
-document.addEventListener("click", () => {window.location.href = "index.html"})
+backButton.addEventListener("click", () => {window.location.href = "index.html"})
 
 
 // Sort the scores so that the list goes highest score to lowest
